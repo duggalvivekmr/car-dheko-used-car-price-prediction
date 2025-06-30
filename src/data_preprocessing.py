ebfd03c340fe45b8cb15cb23cec00d816b6469e8
Vivek Kumar Duggal <duggal.vivekmr@gmail.com>
2025-06-30 17:58:47 +0530

    flattened all nested dict.

---
+++ src/data_preprocessing.py
@@ -0,0 +1,9 @@
+# data preprocessing.py
+import pandas as pd
+import numpy as np  # Import necessary libraries
+import json # Import JSON for handling nested dictionaries  
+import ast  # Import ast for safely evaluating string representations of Python objects
+from pandas import json_normalize
+
+# ------------- 1. Helper Functions -------------
+
